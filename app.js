c70345e06a99c8dbd618de500c9e5be76ca8d0f5
Fasih-developer <fasihjaved997@gmail.com>
2023-11-04 11:12:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// -----------------smoth scroll----------------------
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+//--------load-----------------------
+
+//----------------mouse cursor--------------------- 
+
+let cursor = document.querySelector('.cursor');
+let main = document.querySelector('#main');
+let logo = document.querySelector('.logo');
+let bars = document.querySelector(".ri-align-justify");
+let motto = document.querySelector('.dis-head h2')
+main.addEventListener("mousemove",function(dets){
+    cursor.style.left = dets.x + 'px';
+    cursor.style.top = dets.y + 'px';
+})
+
+bars.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(5)';
+})
+bars.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+})
+logo.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(7)';
+})
+logo.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+})
+motto.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(7)';
+})
+motto.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+})
+let page2 = document.querySelector(".page-2-button button");
+
+page2.addEventListener('mouseenter',function(){
+    cursor.style.display = 'none';
+})
+page2.addEventListener('mouseleave',function(){
+    cursor.style.display = 'inline-block';
+})
+
+//-----------------------video text-----------------------
+let brew = document.querySelector('#brew');
+let bond = document.querySelector('#bond');
+let belong = document.querySelector('#belong');
+
+brew.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(16)';
+    cursor.innerHTML= `<video src="/media/video/1.mp4" autoplay muted loop></video>`
+})
+brew.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+    cursor.innerHTML= ``
+})
+bond.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(16)';
+    cursor.innerHTML= `<video src="/media/video/2.mp4" autoplay muted loop></video>`
+})
+bond.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+    cursor.innerHTML= ``
+})
+belong.addEventListener('mouseenter',function(){
+    cursor.style.transform = 'scale(16)';
+    cursor.innerHTML= `<video src="/media/video/3.mp4" autoplay muted loop></video>`
+})
+belong.addEventListener('mouseleave',function(){
+    cursor.style.transform = 'scale(1)';
+    cursor.innerHTML= ``
+})
+
+//------------------contact video---------------------
+let vid_box =document.querySelector('.contact');
+let video = document.querySelector(".contact-us video");
+
+vid_box.addEventListener('mouseenter',function(){
+    video.style.opacity = '1'
+})
+vid_box.addEventListener('mouseleave',function(){ 
+       video.style.opacity = '0'
+
+})
+
